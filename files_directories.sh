a8802ba697fb15fd1f91078e4776269898110c79
Viraj Kulkarni <viraj.kulkarni14@gmail.com>
2015-10-15 23:44:38 -0700

    Files and directories

---
+++ files_directories.sh
@@ -0,0 +1,10 @@
+# List
+ls
+ls -l
+ls -la
+ls -lah
+# cat and center click to paste
+mkdir "avoid spaces in file/folder names"
+cd
+cd ..
+pwd #print working directory, not present working directory

b868e6a49e4073bd290176f89f2f54ed4c4ab0bf
Viraj Kulkarni <viraj.kulkarni14@gmail.com>
2015-10-15 23:06:29 -0700

    Completed echo and read

---
+++ README.md
@@ -0,0 +1,1 @@
+Shell basics from Kris Occhipinti
---
+++ echo_read.sh
@@ -0,0 +1,9 @@
+echo "Omdatta"
+
+echo "Hello"; echo "World"
+
+echo "What is your name?"; read name
+
+echo "Hello, $name"
+
+echo "What is your name?"; read name; echo "Hello, $name"; echo "Press Enter to Continue"; read

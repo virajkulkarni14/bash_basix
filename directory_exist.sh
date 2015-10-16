ca938bf38a5bb577a17721b51c2da4da79261960
Viraj Kulkarni <viraj.kulkarni14@gmail.com>
2015-10-16 05:20:44 -0700

    checking dpkg

--- directory_exist.sh
+++ directory_exist.sh
@@ -1,3 +1,6 @@
 # Check if directory exists
 
 [ -d "/tmp" ] && echo "The folder exists" || echo "The folder does not exist"
+
+chmod +x mydir.sh
+./mydir.sh
---
+++ program_installed.sh
@@ -0,0 +1,6 @@
+# Check if program installed
+
+dpkg -s package_name # only for Debian based systems
+
+dpkg -s package_name &> /dev/null # taking std output and throwing it away
+

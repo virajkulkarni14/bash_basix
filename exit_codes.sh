364fb19ccf777bf8ea37dbfe1ad45b3be92390b0
Viraj Kulkarni <viraj.kulkarni14@gmail.com>
2015-10-16 05:10:32 -0700

    More on exit codes

--- exit_codes.sh
+++ exit_codes.sh
@@ -1,3 +1,9 @@
 # Exit codes
+x=cat
+echo $x
 [ $x = "cat" ]
 echo $? # check exit code
+# Continuing from prev...
+# && means run following command if prev command ran successfully
+# Shorter form of if else
+[ $x = "cat" ] && echo "x is cat" || echo "Well that's not true"

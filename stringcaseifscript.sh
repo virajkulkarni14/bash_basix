8ce7ed58b923c48967d972d3075a9c3aafd34564
Viraj Kulkarni <viraj.kulkarni14@gmail.com>
2015-10-16 04:06:33 -0700

    which shell

--- stringcaseifscript.sh
+++ stringcaseifscript.sh
@@ -4,10 +4,10 @@ clear
 echo -n "Enter your name: "
 read name
 
-if [ "${name,,}" = "Viraj" ] # NOT working on bash
+if [ "${name,,}" = "Viraj" ] # NOT working on zsh
 then
   echo "Hey buddy!"
-elif [ "${name,,}" = "Sush" ] # NOT working on bash
+elif [ "${name,,}" = "Sush" ] # NOT working on zsh
 then
   echo "Hey bro!"
 else

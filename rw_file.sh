79a2a1d25b4704062d6d9eaa4769e991d406b5e8
Viraj Kulkarni <viraj.kulkarni14@gmail.com>
2015-10-15 23:30:14 -0700

    Read write to files

---
+++ rw_file.sh
@@ -0,0 +1,7 @@
+# read and write to files
+echo "Hello World" > output.txt
+cat output.txt
+# Append to the above file instead of create/overwrite
+echo "Hello Viraj" >> output.txt
+echo -e "This is \nnew" >> output.txt
+echo -n " Hello People" >> output.txt
